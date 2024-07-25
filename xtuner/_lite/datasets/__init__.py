419c0f2c34e0f3a3f62afc4099b4c9e4ba6d078d
HIT-cwh <2892770585@qq.com>
2024-07-25 12:13:20 +0000

    delete useless codes

--- xtuner/_lite/datasets/__init__.py
+++ xtuner/_lite/datasets/__init__.py
@@ -3,8 +3,7 @@ from .format import OPENAI_FORMAT_MAP
 from .llava import (LlavaCollator, LlavaRawDataset, LlavaTokenizedDataset,
                     LlavaTokenizeFunction, SoftPackerForLlava)
 from .load import load_datasets
-from .load_utils import (all_to_all_list, assign_files, barrier,
-                         load_whole_data, mktmpdir, save_part_data)
+from .load_utils import all_to_all_list
 from .text import (HardPackerForText, SoftPackerForText, TextCollator,
                    TextOnlineTokenizeDataset, TextTokenizedDataset,
                    TextTokenizeFunction)
@@ -14,6 +13,5 @@ __all__ = [
     'LlavaTokenizedDataset', 'LlavaTokenizeFunction', 'SoftPackerForLlava',
     'load_datasets', 'HardPackerForText', 'SoftPackerForText', 'TextCollator',
     'TextOnlineTokenizeDataset', 'TextTokenizedDataset',
-    'TextTokenizeFunction', 'load_whole_data', 'assign_files', 'mktmpdir',
-    'save_part_data', 'barrier', 'all_to_all_list'
+    'TextTokenizeFunction', 'all_to_all_list'
 ]
